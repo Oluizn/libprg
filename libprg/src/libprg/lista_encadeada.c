5cae9c429bcd47d8f274b1c4d9aae264aac641b5
Oluizn <antonioluiz.n.eng@gmail.com>
2024-07-09 19:07:38 -0300

    teste

--- libprg/src/libprg/lista_encadeada.c
+++ libprg/src/libprg/lista_encadeada.c
@@ -1,30 +1,30 @@
-// #include "stdio.h"
-// #include "stdlib.h"
-// #include "libprg/libprg.h"
-//
-// numero* criar_no(){
-//     numero* novo = (numero*) malloc(sizeof (numero));
-//     return novo;
-// }
-//
-// numero *inserir_inicio(numero* li, int valor){
-//     numero* novo_no = criar_no();
-//     novo_no->num = valor;
-//     if (li == NULL) {
-//         li = novo_no;
-//         novo_no->prox=NULL;
-//     }
-//     else{
-//         novo_no->prox=li;
-//         li=novo_no;
-//     }
-//     return li;
-// }
-//
-// void imprimir_lista(numero *li){
-//     numero *aux = li;
-//     while (li!=NULL) {
-//         printf("%d\n", aux->num);
-//         aux=aux->prox;
-//     }
-// }
+#include "stdio.h"
+#include "stdlib.h"
+#include "libprg/libprg.h"
+
+numero* criar_no(){
+    numero* novo = (numero*) malloc(sizeof (numero));
+    return novo;
+}
+
+numero *inserir_inicio(numero* li, int valor){
+    numero* novo_no = criar_no();
+    novo_no->num = valor;
+    if (li == NULL) {
+        li = novo_no;
+        novo_no->prox=NULL;
+    }
+    else{
+        novo_no->prox=li;
+        li=novo_no;
+    }
+    return li;
+}
+
+void imprimir_lista(numero *li){
+    numero *aux = li;
+    while (li!=NULL) {
+        printf("%d\n", aux->num);
+        aux=aux->prox;
+    }
+}
