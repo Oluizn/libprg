dc3e93bd0651a88b8874695ba349c6baa90e1c03
Oluizn <antonioluiz.n.eng@gmail.com>
2024-07-10 20:27:01 -0300

    teste

--- libprg/src/libprg/busca_linear.c
+++ libprg/src/libprg/busca_linear.c
@@ -6,3 +6,12 @@ int busca_linear (int vet[], int x, int tam) {
             return i;
     return 0;
 }
+
+int busca_linear_sentinela (int vet[], int x, int tam) {
+    vet[tam] = x;
+    int i = 1;
+    while (vet[i] != x)
+        i++;
+    if (i != tam)
+        
+}
