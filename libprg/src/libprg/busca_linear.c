f213d6cd219f7db5eabb616f416cc6de8b471b76
Oluizn <antonioluiz.n.eng@gmail.com>
2024-07-10 19:47:31 -0300

    teste

--- libprg/src/libprg/busca_linear.c
+++ libprg/src/libprg/busca_linear.c
@@ -1,11 +1,8 @@
 #include "libprg/libprg.h"
 
-
-int busca_linear (int vet[], int x) {
-    for (int i = 0; i < counter; ++i) {
-        if (x == vet[i]) {
+int busca_linear (int vet[], int x, int tam) {
+    for (int i = 0; i < tam; ++i)
+        if (x == vet[i])
             return vet[i];
-        }
-    }
     return 0;
 }
