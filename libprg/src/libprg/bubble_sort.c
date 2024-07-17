476a9cea0ca0112a0aea26a481cd3858f1191d44
Oluizn <antonioluiz.n.eng@gmail.com>
2024-07-17 11:01:24 -0300

    Aula bubble sort

--- libprg/src/libprg/bubble_sort.c
+++ libprg/src/libprg/bubble_sort.c
@@ -6,7 +6,7 @@
     int aux;
     for (int i = 0; i < tamanho; ++i) {
          for (int j = 0; j < tamanho - 1; ++j) {
-             if (vet[j] < vet[j+1]) {
+             if (vet[j] > vet[j+1]) {
                  aux = vet[j];
                  vet[j] = vet[j+1];
                  vet[j+1] = aux;
