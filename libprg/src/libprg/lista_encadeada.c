e4fba6aad9d445c38b4f33280c6b702fdccbbc06
Oluizn <antonioluiz.n.eng@gmail.com>
2024-07-28 12:59:08 -0300

    lista encadeada

--- libprg/src/libprg/lista_encadeada.c
+++ libprg/src/libprg/lista_encadeada.c
@@ -28,8 +28,8 @@ numero *inserir_final(numero* li, int valor) {
         li = novo_no;
     else {
         numero *aux = li;
-        while (aux->prox) {
-            aux->prox++;
+        while (aux) {
+            aux = aux->prox;
         }
         aux = novo_no;
         li = aux;
