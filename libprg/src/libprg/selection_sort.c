b174df5b205572c7ef4a527d36f485950090aee6
Oluizn <antonioluiz.n.eng@gmail.com>
2024-07-29 11:02:17 -0300

    aula

--- libprg/src/libprg/selection_sort.c
+++ libprg/src/libprg/selection_sort.c
@@ -6,14 +6,15 @@ int* selection_sort(int *vet, int tam) {
     for (i = 0; i < tam; i++) {
         min = i;
         for (int j = i + 1; j < tam; j++) {
-            if (vet[j] < vet[min])
+            if (vet[j] < vet[min]) {
                 min = j;
+            }
         }
-    }
-    if (i != min) {
-        int aux = vet[i];
-        vet[i] = vet[min];
-        vet[min] = aux;
+        if (i != min) {
+            int aux = vet[i];
+            vet[i] = vet[min];
+            vet[min] = aux;
+        }
     }
     return vet;
 }
