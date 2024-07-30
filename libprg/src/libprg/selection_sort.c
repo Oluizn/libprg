2c3ca1b9bfa0e71e2416cd6b522e9737e8fba738
Oluizn <antonioluiz.n.eng@gmail.com>
2024-07-30 09:48:30 -0300

    selection sort

--- libprg/src/libprg/selection_sort.c
+++ libprg/src/libprg/selection_sort.c
@@ -2,9 +2,8 @@
 #include "stdio.h"
 
 int* selection_sort(int *vet, int tam) {
-    int i, min;
-    for (i = 0; i < tam; i++) {
-        min = i;
+    for (int i = 0; i < tam; i++) {
+        int min = i;
         for (int j = i + 1; j < tam; j++) {
             if (vet[j] < vet[min]) {
                 min = j;
