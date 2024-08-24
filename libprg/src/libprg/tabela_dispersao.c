37e668806ee2d445fcfe7f4344ab365623343ec4
Oluizn <antonioluiz.n.eng@gmail.com>
2024-08-23 22:07:52 -0300

    implementando arvore exercicio 3 ultimo slide

--- libprg/src/libprg/tabela_dispersao.c
+++ libprg/src/libprg/tabela_dispersao.c
@@ -122,20 +122,20 @@ void imprimir_pessoa(dicionario_t *d, char *chave) {
     }
 }
 
-int main(int argc, char **argv) {
-    dicionario_t *dicionario = criar_dicionario(5);
-    if (dicionario == NULL) {
-        printf("Não foi possível reservar memória\n");
-        exit(EXIT_FAILURE);
-    }
-    pessoa_t *p = malloc(sizeof(pessoa_t));
-    // https://en.cppreference.com/w/c/string/byte/strdup
-    // strdup reserva memória para fazer a cópia da string. Presente no padrão C23
-    p->cpf = strdup("123");
-    p->nome = strdup("Juca");
-    p->email = strdup("juca@example.ogr");
-    inserir(dicionario, p->cpf, p);
-    imprimir_pessoa(dicionario, "123");
-    destruir_dicionario(dicionario);
-    return 0;
-}
+// int main(int argc, char **argv) {
+//     dicionario_t *dicionario = criar_dicionario(5);
+//     if (dicionario == NULL) {
+//         printf("Não foi possível reservar memória\n");
+//         exit(EXIT_FAILURE);
+//     }
+//     pessoa_t *p = malloc(sizeof(pessoa_t));
+//     // https://en.cppreference.com/w/c/string/byte/strdup
+//     // strdup reserva memória para fazer a cópia da string. Presente no padrão C23
+//     p->cpf = strdup("123");
+//     p->nome = strdup("Juca");
+//     p->email = strdup("juca@example.ogr");
+//     inserir(dicionario, p->cpf, p);
+//     imprimir_pessoa(dicionario, "123");
+//     destruir_dicionario(dicionario);
+//     return 0;
+// }
